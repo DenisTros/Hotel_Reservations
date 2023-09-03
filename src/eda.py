7ba353a825ec4517e06831f085f0c145547fc2d8
Denis <denistrosman@gmail.com>
2023-09-03 17:35:17 +0200

    edits

--- src/eda.py
+++ src/eda.py
@@ -11,7 +11,7 @@ import math
 #Data viz
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+# import plotly.express as px
 color_pal = sns.color_palette()
 plt.style.use('fivethirtyeight')
 
@@ -34,3 +34,28 @@ def valores_unicos(df,columna):
     data_frame['share']=data_frame[columna]/sum(data_frame[columna])*100
     data_frame.columns = ['count','share']
     return data_frame
+
+def agrupar_categoricas(df):
+    """ Agrupar variables categoricas"""
+    df['room_type_reserved']=[x if x in ['Room_Type 1','Room_Type 4'] else 'other' for x in df.room_type_reserved]
+    df['type_of_meal_plan']=[x if x in ['Meal Plan 1','Not Selected'] else 'other' for x in df.type_of_meal_plan]
+    df['market_segment_type']=[x if x in ['Offline','Online'] else 'other' for x in df.market_segment_type]
+
+    return df
+
+def Generate_heatmap_graph(corr, chart_title, mask_uppertri=False ):
+    
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = mask_uppertri
+    fig,ax = plt.subplots(figsize=(10,10))
+    ax.set_facecolor('white')
+    sns.heatmap(corr
+                , mask = mask
+                , square = True
+                , annot = True
+                , annot_kws={'size': 10.5, 'weight' : 'bold'}
+                , cmap=plt.get_cmap("YlOrBr")
+                , linewidths=.1)
+    plt.title(chart_title, fontsize=14)
+
+    plt.show()
